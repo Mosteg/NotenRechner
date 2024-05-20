6b98e2e261f952dc93a2a51d34fc470d42ac9829
Dein Name <deine.email@example.com>
2024-05-20 14:33:41 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: '/assets/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        open: true,
+        port: 9000,
+        hot: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+};
